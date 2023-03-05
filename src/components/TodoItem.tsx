10b42d52d4c575df6af3368dd64d5fffb6bf3cfd
Behemoth <kodohdaniel@gmail.com>
2023-03-04 19:19:13 -0600

    reached mvp

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,33 @@
+import styles from "./TodoItem.module.css";
+
+type TodoItem = {
+  Id: string;
+  Task: string;
+  completed: boolean;
+};
+
+type TodoItemProps = {
+  item: TodoItem;
+  className?: string;
+  deleteItem: (id: string) => void;
+  completeItem: (id: string) => void;
+};
+
+function TodoItem({
+  item,
+  className,
+  completeItem,
+  deleteItem,
+}: TodoItemProps) {
+  return (
+    <div className={`${className} ${styles.ItemWrapper}`}>
+      <p>{item.Task}</p>
+      <div className={styles.ButtonWrapper}>
+        <button onClick={() => completeItem(item.Id)}>complete</button>
+        <button onClick={() => deleteItem(item.Id)}>delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,53 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+import TodoItem from "@/components/TodoItem";
+import TaskInput from "@/components/TaskInput";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+let defaultTodoItems: TodoItem[] = [
+  {
+    Id: "First",
+    Task: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus faucibus nulla, ut molestie enim pellentesque nec. Nam pellentesque varius libero, a placerat nunc imperdiet a. Proin vestibulum purus vel purus pellentesque consectetur ac i",
+    completed: false,
+  },
+  {
+    Id: "Second",
+    Task: "Nulla at feugiat sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean bibendum urna diam, eu elementum ex scelerisque ac. Mauris sodales quam vel nisi molestie, quis cursus ligula fermentum. Nulla vel tellus eget ante mollis suscipit nec vel erat. ",
+    completed: false,
+  },
+];
 
 export default function Home() {
+  let [todoItems, setTodoItems] = useState<TodoItem[]>(defaultTodoItems);
+
+  let deleteItem = (itemId: string) => {
+    setTodoItems((items) => {
+      let filteredList = items.filter((item) => item.Id != itemId);
+      return [...filteredList];
+    });
+  };
+
+  let completeItem = (itemId: string) => {
+    setTodoItems((items) => {
+      let filteredList = items.map((item) => {
+        if (item.Id != itemId) return item;
+
+        return { ...item, completed: true };
+      });
+      return [...filteredList];
+    });
+  };
+
+  let createItem = (item: TodoItem) => {
+    setTodoItems((items) => {
+      return [...items, item];
+    });
+  };
+
   return (
     <>
       <Head>
@@ -15,109 +57,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+        <div className={styles.AppHeader}>
+          <h1>Todo</h1>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <div className={styles.Input}>
+          <TaskInput createItem={createItem}></TaskInput>
+          <div className={styles.HorizontalLine}></div>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className={styles.ItemsContainer}>
+          {todoItems.map((item) => (
+            <TodoItem
+              item={item}
+              completeItem={completeItem}
+              deleteItem={deleteItem}
+            ></TodoItem>
+          ))}
         </div>
       </main>
     </>
-  )
+  );
 }
