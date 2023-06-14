1611f548888c6fca422a3e65a68625a7516cf252
Behemoth <kodohdaniel@gmail.com>
2023-06-14 09:08:42 -0500

    Starter project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,49 +4,10 @@ import { Inter } from "next/font/google";
 import { motion } from "framer-motion";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
-import TodoItem from "@/components/TodoItem";
-import TaskInput from "@/components/TaskInput";
 import { AnimatePresence, MotionConfig } from "framer-motion";
 
-let defaultTodoItems: TodoItem[] = [
-  {
-    Id: "First",
-    Task: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus faucibus nulla, ut molestie enim pellentesque nec. Nam pellentesque varius libero, a placerat nunc imperdiet a. Proin vestibulum purus vel purus pellentesque consectetur ac i",
-    completed: false,
-  },
-  {
-    Id: "Second",
-    Task: "Nulla at feugiat sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean bibendum urna diam, eu elementum ex scelerisque ac. Mauris sodales quam vel nisi molestie, quis cursus ligula fermentum. Nulla vel tellus eget ante mollis suscipit nec vel erat. ",
-    completed: false,
-  },
-];
 
 export default function Home() {
-  let [todoItems, setTodoItems] = useState<TodoItem[]>(defaultTodoItems);
-
-  let deleteItem = (itemId: string) => {
-    setTodoItems((items) => {
-      let filteredList = items.filter((item) => item.Id != itemId);
-      return [...filteredList];
-    });
-  };
-
-  let completeItem = (itemId: string) => {
-    setTodoItems((items) => {
-      let filteredList = items.map((item) => {
-        if (item.Id != itemId) return item;
-
-        return { ...item, completed: true };
-      });
-      return [...filteredList];
-    });
-  };
-
-  let createItem = (item: TodoItem) => {
-    setTodoItems((items) => {
-      return [item, ...items];
-    });
-  };
 
   return (
     <>
@@ -61,28 +22,8 @@ export default function Home() {
           <h1>Todo</h1>
         </div>
         <div className={styles.Input}>
-          <TaskInput createItem={createItem}></TaskInput>
-          <div className={styles.HorizontalLine}></div>
         </div>
         <div className={styles.ItemsContainer}>
-          <AnimatePresence mode="popLayout">
-            {todoItems.map((item) => (
-              <motion.div
-                layout
-                layoutScroll
-                key={item.Id}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <TodoItem
-                  item={item}
-                  completeItem={completeItem}
-                  deleteItem={deleteItem}
-                ></TodoItem>
-              </motion.div>
-            ))}
-          </AnimatePresence>
         </div>
       </main>
     </>
